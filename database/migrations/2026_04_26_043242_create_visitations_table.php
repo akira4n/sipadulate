cee84be941ac9969915e29f326cc16eab95da0b4
akira4n <yobiijo94@gmail.com>
2026-05-10 18:21:22 +0700

    feat: add nomor_antrian to visitations table

--- database/migrations/2026_04_26_043242_create_visitations_table.php
+++ database/migrations/2026_04_26_043242_create_visitations_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('visitations', function (Blueprint $table) {
             $table->id();
             $table->string('qr_code');
+            $table->integer('nomor_antrian');
             $table->enum('jenis_wbp', ['tahanan', 'narapidana']);
             $table->date('tanggal_kunjungan');
             $table->enum('jenis_identitas', ['nik', 'sim', 'passport', 'nisn', 'lainnya']);
