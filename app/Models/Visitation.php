cad296c427b40fec0241f19ba18b5ae50df946d3
akira4n <yobiijo94@gmail.com>
2026-05-13 01:37:17 +0700

    fix(model): update fillable properties in Visitation model

--- app/Models/Visitation.php
+++ app/Models/Visitation.php
@@ -7,7 +7,22 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Visitation extends Model
 {
-    protected $guarded = ['id'];
+    protected $fillable = [
+        'qr_code',
+        'nomor_antrian',
+        'jenis_wbp',
+        'tanggal_kunjungan',
+        'jenis_identitas',
+        'nama_pengunjung',
+        'nomor_identitas',
+        'no_hp',
+        'alamat',
+        'nama_wbp',
+        'hubungan_wbp',
+        'jumlah_pengikut',
+        'foto_pegang_identitas',
+        'foto_identitas',
+    ];
 
     protected $casts = [
         'tanggal_kunjungan' => 'date',
